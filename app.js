6a4f47cd8d280f4bfd23ed5354e57062e0fcac34
Johnnysky <vanpasie20@gmail.com>
2025-10-27 16:27:51 -0700

    the event module

--- app.js
+++ app.js
@@ -1,5 +1,33 @@
-var stuff = require('./stuff');
+var events = require('events');
 
-console.log(stuff.counter(['johnny', 'crystal', 'sky']));
-console.log(stuff.adder(5,6));
-console.log(stuff.adder(stuff.pi, 5));
+// var myEmitter = new events.EventEmitter();
+
+// myEmitter.on('someEvent', function(msg) {
+//     console.log(msg);
+// });
+
+// myEmitter.emit('someEvent', 'the event was emitted!');
+
+var util = require('util');
+
+var Person = function(name) {
+    this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+
+var james = new Person('james');
+var mary = new Person('mary');
+var ryu = new Person('ryu');
+
+var people = [james, mary, ryu];
+
+people.forEach(function(person) {
+    person.on('speak', function(msg) {
+        console.log(person.name + ' said: ' + msg);
+    });
+});
+
+james.emit('speak', 'hey dudes');
+ryu.emit('speak', 'I want a curry');
+mary.emit('speak', 'hello world');
