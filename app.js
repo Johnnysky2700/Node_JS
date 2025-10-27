071526363f28de440ccf4362fb33e38be632a9dc
Johnnysky <vanpasie20@gmail.com>
2025-10-27 04:53:51 -0700

    Function Expression

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-// // setTimeout(function() {
-// //     console.log('3 seconds have passed');
-// // }, 3000);
+// 
 
-// var time = 0;
+function callFunction(fun){
+    fun();
+}
 
-// // setInterval(function() {
-// //     time += 2;
-// //     console.log(time + ' seconds have passed');
-// // }, 2000);
+//function expression
+var sayBye = function(){
+    console.log("Bye");
+};
 
-// var timer = setInterval(function() {
-//     time += 2;
-//     console.log(time + ' seconds have passed');
-//     if (time > 5) {
-//         clearInterval(timer);
-//     }
-// }, 2000);
-
-console.log(__dirname);
-console.log(__filename);
-
-// require, module, exports
+callFunction(sayBye);
