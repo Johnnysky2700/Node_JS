9be7fec47e303ba7d7ee5a8b410fdb69120ce8dd
Johnnysky <vanpasie20@gmail.com>
2025-10-30 16:02:47 -0700

    creating a partial views

--- app.js
+++ app.js
@@ -4,16 +4,12 @@ var app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.render('index');
 });
 
 app.get('/contact', function (req, res) {
-  res.sendFile(__dirname + '/contact.html');
+  res.render('contact');
 });
-
-// app.get('/profile/:name', function (req, res) {
-//   res.render('profile', { person: req.params.name });
-// });
 
 app.get('/profile/:name', function (req, res) {
   var data = { age: 26, job: 'website design', hobbies: ['eating', 'sleeping', 'coding'] };
