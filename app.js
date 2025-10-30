517ddd8d1ac3b9973b47c7b17ec055595a33e940
Johnnysky <vanpasie20@gmail.com>
2025-10-30 04:55:02 -0700

    how to inject data into views and serve those views to the user using Templating Engines

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 var express = require('express');
-
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('This is the homepage');
+  res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/contact', function (req, res) {
-  res.send('This is the contact page');
+  res.sendFile(__dirname + '/contact.html');
 });
 
-// app.get('/profile/:id', function (req, res) {
-//   res.send('You requested to see a profile with the id of ' + req.params.id);
+// app.get('/profile/:name', function (req, res) {
+//   res.render('profile', { person: req.params.name });
 // });
 
 app.get('/profile/:name', function (req, res) {
-  res.send('You requested to see a profile with the name of ' + req.params.name);
+  var data = { age: 26, job: 'website design'}
+  res.render('profile', { person: req.params.name, data: data });
 });
 
 app.listen(3000);
