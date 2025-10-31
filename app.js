8bd747af99db66b3446db98a1ad87db98b8559ea
Johnnysky <vanpasie20@gmail.com>
2025-10-31 05:15:51 -0700

    created app.js for engine point, set up app using express, set up view engine, set up static file using build in middleware into express called express.static, then created controller lists to handle route and render of views passing of data to views separating it as a module (To-do App (part 2))

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 var express = require('express');
-var bodyParser = require('body-parser');
+var todoController = require('./controllers/todoController');
+
 var app = express();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
+//set up template engine
 app.set('view engine', 'ejs');
-app.use('/assets', express.static('stuff'));
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
+//static files or middleware
+app.use(express.static('./public'));
 
-app.get('/contact', function (req, res) {
-  res.render('contact', {qs: req.query});
-});
+//fire controllers
+todoController(app);
 
-app.post('/contact', urlencodedParser, function (req, res) {
-  console.log(req.body);
-  res.render('contact-success', {data: req.body});
-});
-
-app.get('/profile/:name', function (req, res) {
-  var data = { age: 26, job: 'website design', hobbies: ['eating', 'sleeping', 'coding'] };
-  res.render('profile', { person: req.params.name, data: data });
-});
-
+//listen to port
 app.listen(3000);
+console.log('You are listening to port 3000');
---
+++ controllers/todoController.js
@@ -0,0 +1,15 @@
+module.exports = function(app){
+
+    //set up controller page
+    app.get('/todo', function(req, res){
+        
+    });
+
+    app.post('/todo', function(req, res){
+        
+    });
+
+    app.delete('/todo/:item', function(req, res){
+      
+    });
+}
