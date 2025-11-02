442bed7f7d07983048c1604f6c4983e137dbca34
Johnnysky <vanpasie20@gmail.com>
2025-11-02 08:45:44 -0800

    Making a To-Do App (part 3)

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,7 +2,7 @@ module.exports = function(app){
 
     //set up controller page
     app.get('/todo', function(req, res){
-        
+        res.render('todo');
     });
 
     app.post('/todo', function(req, res){
