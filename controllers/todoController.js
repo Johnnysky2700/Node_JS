da66d965e500d54529e058037d78010349314dc7
Johnnysky <vanpasie20@gmail.com>
2025-11-02 16:10:50 -0800

    sucessfully created database on mongodb connect to installed mongoose, then created a schma for blueprint mongodb expect

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,22 +1,47 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
-var urlencodedParser = bodyParser.urlencoded({extented: false}); //middleware
+// Connect to the database
+mongoose.connect('mongodb+srv://test:test@test.ej2tlvl.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
 
-module.exports = function(app){
+// Create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
 
-    //set up controller page
-    app.get('/todo', function(req, res){
-        res.render('todo', {todos: data});
+var Todo = mongoose.model('Todo', todoSchema);
+
+// Save an initial item
+var itemOne = new Todo({ item: 'buy flowers' });
+itemOne.save()
+    .then(() => console.log('item saved'))
+    .catch(err => console.error('Error saving item:', err));
+
+var data = [{ item: 'get milk' }, { item: 'walk dog' }, { item: 'kick some coding ass' }];
+var urlencodedParser = bodyParser.urlencoded({ extended: false }); // middleware
+
+module.exports = function(app) {
+
+    // Set up controller page
+    app.get('/todo', function(req, res) {
+        res.render('todo', { todos: data });
     });
 
-    app.post('/todo', urlencodedParser, function(req, res){
-        data.push(req.body);
-        res.json(data);
+    app.post('/todo', urlencodedParser, function(req, res) {
+        var newTodo = new Todo(req.body);
+        newTodo.save()
+            .then(() => {
+                data.push(req.body);
+                res.json(data);
+            })
+            .catch(err => {
+                console.error('Error saving todo:', err);
+                res.status(500).send('Error saving todo');
+            });
     });
 
-    app.delete('/todo/:item', function(req, res){
-        data = data.filter(function(todo){
+    app.delete('/todo/:item', function(req, res) {
+        data = data.filter(function(todo) {
             return todo.item.replace(/ /g, '-') !== req.params.item;
         });
         res.json(data);
