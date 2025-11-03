444d3e69fe7080a164cc731c113edc7a448d80a6
Johnnysky <vanpasie20@gmail.com>
2025-11-02 17:03:36 -0800

    finally i can create a server connected to mongodb which can get, post and delete user data (To-do App part 6)

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,7 +2,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 // Connect to the database
-mongoose.connect('mongodb+srv://test:test@test.ej2tlvl.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb+srv://test:test@test.ej2tlvl.mongodb.net/');
 
 // Create a schema - this is like a blueprint
 var todoSchema = new mongoose.Schema({
@@ -11,39 +11,45 @@ var todoSchema = new mongoose.Schema({
 
 var Todo = mongoose.model('Todo', todoSchema);
 
-// Save an initial item
-var itemOne = new Todo({ item: 'buy flowers' });
-itemOne.save()
-    .then(() => console.log('item saved'))
-    .catch(err => console.error('Error saving item:', err));
-
-var data = [{ item: 'get milk' }, { item: 'walk dog' }, { item: 'kick some coding ass' }];
-var urlencodedParser = bodyParser.urlencoded({ extended: false }); // middleware
+// var data = [{ item: 'get milk' }, { item: 'walk dog' }, { item: 'kick some coding ass' }];
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app) {
 
     // Set up controller page
-    app.get('/todo', function(req, res) {
-        res.render('todo', { todos: data });
+    app.get('/todo', async (req, res) => {
+        //get data from mongodb and pass it to view
+        try {
+            const data = await Todo.find({});
+            res.render('todo', { todos: data });
+        } catch (err) {
+            console.error(err);
+            res.status(500).send('Error fetching todos');
+        }
     });
 
-    app.post('/todo', urlencodedParser, function(req, res) {
-        var newTodo = new Todo(req.body);
-        newTodo.save()
-            .then(() => {
-                data.push(req.body);
-                res.json(data);
-            })
-            .catch(err => {
-                console.error('Error saving todo:', err);
-                res.status(500).send('Error saving todo');
-            });
+    app.post('/todo', urlencodedParser, async (req, res) => {
+        //get data from the view and add it to mongodb
+        try {
+            const newTodo = new Todo(req.body);
+            const data = await newTodo.save();
+            res.json(data);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send('Error saving todo');
+        }
     });
 
-    app.delete('/todo/:item', function(req, res) {
-        data = data.filter(function(todo) {
-            return todo.item.replace(/ /g, '-') !== req.params.item;
-        });
-        res.json(data);
+    app.delete('/todo/:item', async (req, res) => {
+        //delete the requested item from mongodb
+        try {
+            const data = await Todo.findOneAndDelete(
+                { item: req.params.item.replace(/\-/g, " ") }
+            );
+            res.json(data);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send('Error deleting todo');
+        }
     });
 }
